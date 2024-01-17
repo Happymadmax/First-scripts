dff230378fcb6d9f9446bd5c01e72ba1a9fd467a
Happymadmax <156545355+Happymadmax@users.noreply.github.com>
2024-01-17 19:39:17 +0000

    Add files via upload

---
+++ blips/config.lua
@@ -0,0 +1,120 @@
+Config = Config or {}
+
+---ANPR Blips  
+Config.blipsShow = true
+
+Config.Locations = {
+[1] = {
+    vector = vector3(223.00,
+     -1042.60,
+      29.12), 
+    text = "ANPR", 
+    color = 5, 
+    sprite = 184, 
+    scale = 0.5,
+},
+[2] = {
+    vector = vector3(104.11,
+     -1363.77,
+      29.10), 
+    text = "ANPR", 
+    color = 5, 
+    sprite = 184, 
+    scale = 0.5,
+},
+[3] = {
+    vector = vector3(-127.66,
+     -1738.60, 
+     29.89),
+    text = "ANPR", 
+    color = 5, 
+    sprite = 184, 
+    scale = 0.5,
+},
+[4] = {
+    vector = vector3(-763.85,
+     -2182.47, 
+     15.26),
+    text = "ANPR", 
+    color = 5, 
+    sprite = 184, 
+    scale = 0.5,
+},
+[5] = {
+    vector = vector3(-770.00,
+     -1116.10,
+     10.46),
+    text = "ANPR", 
+    color = 5, 
+    sprite = 184, 
+    scale = 0.5,
+},
+[6] = {
+    vector = vector3(-1038.81,
+    -192.10,
+    37.62),
+    text = "ANPR", 
+    color = 5, 
+    sprite = 184, 
+    scale = 0.5,
+},
+[7] = {
+    vector = vector3(470.93,
+     -311.69,
+     46.85),
+    text = "ANPR", 
+    color = 5, 
+    sprite = 184, 
+    scale = 0.5,
+},
+[8] = {
+    vector = vector3(814.19,
+     -2623.73,
+     52.42),
+    text = "ANPR", 
+    color = 5, 
+    sprite = 184, 
+    scale = 0.5,
+},
+[9] = {
+    vector = vector3(-498.72,
+     -2265.92,
+     61.43),
+    text = "ANPR", 
+    color = 5, 
+    sprite = 184, 
+    scale = 0.5,
+},
+[10] = {
+    vector = vector3(1573.21,
+     -983.65,
+     59.78),
+    text = "ANPR", 
+    color = 5, 
+    sprite = 184, 
+    scale = 0.5,
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ blips/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+
+author 'Max'
+description 'Blip Creator Free For QBCore and ESX'
+version '1.0'
+scriptname 'blips'
+
+
+
+shared_script 'config.lua'
+client_script 'blips.lua'
